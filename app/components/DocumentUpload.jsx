0b9b62f445c3c75936cec70932383ec8c8a5ec23
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-03-28 09:12:23 -0400

    remove logs

--- app/components/DocumentUpload.jsx
+++ app/components/DocumentUpload.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 import { supabase } from '../lib/supabase'
 import OpenAI from "openai";
-console.log(process.env.OPENAI_API_KEY)
 
 const openai = new OpenAI( {apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser: true } );
 
