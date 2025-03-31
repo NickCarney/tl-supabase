9512719914cd14d811c26b6bfe2c6b0289a62603
Nick Carney <nickcarney@Nicks-MacBook-Air.local>
2025-03-31 12:30:23 -0400

    callback to homepage

---
+++ supa/app/auth/callback/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { supabase } from '@/lib/supabaseClient';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function AuthCallback() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleAuth = async () => {
+      await supabase.auth.getSession();
+      router.push('/');
+    };
+
+    handleAuth();
+  }, [router]);
+
+  return <div>Loading...</div>;
+}
