7ca242da89991e1569d943032c3e2fe689915488
lucas.tolosa <lucas.tolosa@novadba.com>
2025-05-29 15:37:32 -0300

    added authentication with auth0

---
+++ supa/middleware.ts
@@ -0,0 +1,32 @@
+
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { auth0 } from "./lib/auth0";
+
+export async function middleware(request: NextRequest) {
+  const session = await auth0.getSession(request);  
+  if (request.nextUrl.pathname.startsWith("/auth")) {
+    return await auth0.middleware(request);
+  }
+  if (!session || !session.user) {
+    const loginUrl = new URL("/auth/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  return await auth0.middleware(request);
+}
+
+
+  
+
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
